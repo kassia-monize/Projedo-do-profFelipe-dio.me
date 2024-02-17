ccc36bbdbf00431df414635da4a970a5b0ae7a1d
kassia-monize <kassiamonize@hotmail.com>
2024-02-16 22:51:31 -0300

    meu desafio

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-console.log("oi");
+let nome = "Castello";
+let XP = 7500
+let Nivel;
+let cobtador = 0
+
+switch(true){
+    case(XP <= 1000):
+    Nivel = "Ferro";
+    breack;
+
+    case(XP <= 2000 && XP > 1000):
+    Nivel = "Bronze";
+    break;
+
+    case(XP <= 5000 && XP > 2000);
+    Nivel = "Prata";
+    break;
+
+    case(XP <= 7000 && XP > 5000);
+    Nivel = "Ouro";
+    break;
+
+    case(XP <= 8000 && XP > 7000);
+    Nivel = "Platina";
+    break;
+
+    case(XP <= 9000 && XP > 8000);
+    Nivel = "Ascendente";
+    break;
+
+    case(XP <= 10000 && XP > 9000);
+    Nivel = "Imortal"
+    break;
+
+    default:
+        console.log("Radiante");
+}
+console.log("Olá " + nome + "! Nesta fase você realizou " + XP + " pontos, chegando ao Nível " + Nivel + "!")
+while (contador < 1){
+    console.log("Parabéns " + nome + "! Você está no reino Portal dos Nômades!!")
+    contador++
+}
+
+
 
